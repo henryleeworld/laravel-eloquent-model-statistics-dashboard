7af12f6ecd7ba619155b3ce91af1546e995c6d5d
Henry Lee <henryleeworld@gmail.com>
2021-07-08 06:55:48 +0800

    feat:初始化

---
+++ README.md
@@ -0,0 +1,30 @@
+# Laravel 8 Eloquent 模型統計儀表板
+
+引入 jhumanj 的 laravel-model-stats 套件來擴增 Eloquent 模型統計儀表板，儀表板以資訊圖像化方式呈現，能快速瞭解模型統計資料。
+
+## 使用方式
+- 把整個專案複製一份到你的電腦裡，這裡指的「內容」不是只有檔案，而是指所有整個專案的歷史紀錄、分支、標籤等內容都會複製一份下來。
+```sh
+$ git clone
+```
+- 將 __.env.example__ 檔案重新命名成 __.env__，如果應用程式金鑰沒有被設定的話，你的使用者 sessions 和其他加密的資料都是不安全的！
+- 當你的專案中已經有 composer.lock，可以直接執行指令以讓 Composer 安裝 composer.lock 中指定的套件及版本。
+```sh
+$ composer install
+```
+- 產生 Laravel 要使用的一組 32 字元長度的隨機字串 APP_KEY 並存在 .env 內。
+```sh
+$ php artisan key:generate
+```
+- 執行 __Artisan__ 指令的 __migrate__ 來執行所有未完成的遷移，並執行資料庫填充（如果要測試的話）。
+```sh
+$ php artisan migrate --seed
+```
+- 在瀏覽器中輸入已定義的路由 URL 來訪問，例如：http://127.0.0.1:8000。
+- 你可以經由 `/stats` 來進行統計儀表板建立及瀏覽。
+
+----
+
+## 畫面截圖
+![](https://i.imgur.com/7JVAPfg.png)
+> 可根據所設定的聚集類型產生模型中的統計資料
---
+++ resources/views/vendor/model-stats/dashboard.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Meta Information -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+{{--    <link rel="shortcut icon" href="{{ asset('/vendor/telescope/favicon.ico') }}">--}}
+
+    <meta name="robots" content="noindex, nofollow">
+
+    <title>{{ config('app.name') ? config('app.name') : '' }}</title>
+
+    <!-- Style sheets-->
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="{{ asset(mix('app.css', 'vendor/model-stats')) }}" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="model-stats" v-cloak>
+{{--    <alert :message="alert.message"--}}
+{{--           :type="alert.type"--}}
+{{--           :auto-close="alert.autoClose"--}}
+{{--           :confirmation-proceed="alert.confirmationProceed"--}}
+{{--           :confirmation-cancel="alert.confirmationCancel"--}}
+{{--           v-if="alert.type"></alert>--}}
+
+    <div class="max-w-2xl mx-auto mb-5">
+        <div class="flex items-center py-4">
+            <h1 class="mb-0 ml-3 font-medium text-2xl">{{ config('app.name') ? config('app.name') : '' }}</h1>
+        </div>
+
+        <div class="flex w-full mt-4">
+            <div class="col-10">
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Global ModelStats Object -->
+<script>
+    window.ModelStats = {
+        config: @json($config),
+        models: @json($models)
+    }
+</script>
+
+<script src="{{ asset(mix('app.js', 'vendor/model-stats')) }}"></script>
+</body>
+</html>
